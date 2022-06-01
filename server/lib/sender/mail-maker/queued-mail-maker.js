938358fa283cac01cbf969fd9ad5aa37b327cb2b
Erik Kučák <riko98@azet.sk>
2022-06-01 08:54:51 +0200

    Added queued mail maker

---
+++ server/lib/sender/mail-maker/queued-mail-maker.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const MailMaker = require('./mail-maker');
+
+/**
+ * The class which inherits from MailSender and is responsible for making mails of QUEUED messages.
+ */
+class QueuedMailMaker extends MailMaker {
+    async makeMail(messageData) {
+        const subData = {
+            subscriptionId: messageData.subscriptionId,
+            listId: messageData.listId,
+            to: messageData.to,
+            mergeTags: messageData.mergeTags,
+            encryptionKeys: messageData.encryptionKeys
+        };
+        return await super.makeMail(subData);
+    }
+
+    /* Make string of receiver, used only for debugging purpose. */
+    makeTarger(messageData) {
+        let target = '';
+
+        if (messageData.listId && messageData.subscriptionId) {
+            target = `${messageData.listId}:${messageData.subscriptionId}`;
+        } else if (messageData.to) {
+            if (messageData.to.name && messageData.to.address) {
+                target = `${messageData.to.name} <${messageData.to.address}>`;
+            } else if (messageData.to.address) {
+                target = messageData.to.address;
+            } else {
+                target = messageData.to.toString();
+            }
+        }
+
+        return target;
+    }
+}
+
+module.exports = QueuedMailMaker;

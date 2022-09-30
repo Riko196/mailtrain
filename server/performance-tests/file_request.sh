6fe258fe0a37f4927196da06fc67d89f2903910b
Erik Kučák <riko196@gmail.com>
2022-09-30 13:51:09 +0200

    Added file_request script

---
+++ server/performance-tests/file_request.sh
@@ -0,0 +1,9 @@
+# URL for file request (HA)PUBLIC server and REST API argument (set your value)
+file_url=http://localhost:8001/files/campaign/file/1/6e49502594b81c1e1397d565747f1af9
+#file_url=http://parlab.ms.mff.cuni.cz:7020/files/campaign/file/1/6e49502594b81c1e1397d565747f1af9
+
+# File requests count
+request_count=100
+
+# Execute ${request_count} parallel file requests and store them in /dev/null (deleting)
+seq 1 ${request_count} | xargs -I{} -n 1 -P ${request_count} curl "${file_url}" -o "/dev/null"
